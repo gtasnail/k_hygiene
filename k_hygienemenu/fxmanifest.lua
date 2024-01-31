f057f5b4bc78f2c2c7f1003080cfae4dba940e0d
Jumanji <100861025+gtasnail@users.noreply.github.com>
2024-01-31 03:22:06 +0000

    Upload Files

---
+++ k_hygienemenu/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Kypos'
+
+discription 'Simple and basic self disease check for HygieneV'
+version '1.0.0'
+
+
+
+client_scripts {
+    '@ox_lib/init.lua',
+    '@k_hygiene/config.lua',
+    'client/*.lua'
+}
+
+
+lua54 'yes'
+
